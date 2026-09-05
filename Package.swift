9504084d347ddea138d3f682545cebbb8b1dc901
Den Andreychuk <denis.andrei4uk@gmail.com>
2026-09-05 22:16:44 +0300

    Depend on the Pimine SwiftUIX fork

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vmanot/Merge.git", branch: "master"),
-        .package(url: "https://github.com/SwiftUIX/SwiftUIX.git", branch: "master"),
+        .package(url: "https://github.com/Pimine/SwiftUIX.git", branch: "master"),
     ],
     targets: [
         .target(
